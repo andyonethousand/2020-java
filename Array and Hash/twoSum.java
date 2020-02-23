890690b643b9b8a8ab60d87fde482afb1132b54f
unmatchedTC <54973763+andyonethousand@users.noreply.github.com>
2020-02-23 20:17:58 +0100

    Create twoSum.java

---
+++ Array and Hash/twoSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hash_table = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            hash_table.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int number_to_get = target - nums[i];
+            if (hash_table.containsKey(number_to_get) && hash_table.get(number_to_get) != i) {
+                return new int[] {i, hash_table.get(number_to_get)};
+            }
+        } 
+    throw new IllegalArgumentException("No two sum solution");
+    }
+}            
